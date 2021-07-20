505abe764baf175f7d2e18a5bb180ceb946e40ef
Gabriel Ferreira <gabriel@jostle.me>
2021-07-19 21:55:08 -0700

    Implemented Video/GET one item endpoint

--- src/main/java/com/gabrielpf/alurabackendchallange/controller/Video.java
+++ src/main/java/com/gabrielpf/alurabackendchallange/controller/Video.java
@@ -1,8 +1,11 @@
 package com.gabrielpf.alurabackendchallange.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,14 @@ public class Video {
     public List<VideosVoOut> list() {
         return videosRepo.findAll().stream().map(VideosVoOut::new).toList();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VideosVoOut> getOne(@PathVariable("id") UUID id) {
+        return videosRepo.findById(id)
+                .map(VideosVoOut::new)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 
 }
