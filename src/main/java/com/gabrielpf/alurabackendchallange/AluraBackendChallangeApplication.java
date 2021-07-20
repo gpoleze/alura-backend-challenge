cbfb48fbba25b22130f99dee2b17daf807322458
Gabriel Ferreira <gabriel@jostle.me>
2021-07-19 19:51:43 -0700

    Initial commit

---
+++ src/main/java/com/gabrielpf/alurabackendchallange/AluraBackendChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielpf.alurabackendchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraBackendChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraBackendChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrielpf/alurabackendchallange/AluraBackendChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielpf.alurabackendchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraBackendChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
