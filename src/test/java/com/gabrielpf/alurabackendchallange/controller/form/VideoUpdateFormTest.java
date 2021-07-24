892641859bad70e25afb6a5d5dead8054d811964
Gabriel Ferreira <gabriel@jostle.me>
2021-07-23 23:56:43 -0700

    Fixing naming

--- src/test/java/com/gabrielpf/alurabackendchallange/controller/form/VideoUpdateFormTest.java
+++ src/test/java/com/gabrielpf/alurabackendchallange/controller/form/VideoUpdateFormTest.java
@@ -2,7 +2,6 @@ package com.gabrielpf.alurabackendchallange.controller.form;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
